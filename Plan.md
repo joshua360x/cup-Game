edba9db5a47afe046b952a08124a725038a50f9c
joshua360x <jkw5499@psu.edu>
2021-11-30 20:05:57 -0500

    game prepared + hard reset btn

---
+++ Plan.md
@@ -0,0 +1,51 @@
+
+# HTML Setup
+
+- 3 div containing images of cups 
+    * why
+        - we need to show the user which cup is the ball under 
+    * how 
+        - we can replace the images with the divs in each cup 
+
+- 3 buttons for the cup to sense that it is clicked upon 
+    * why
+        - so we are able to tell when and what cup is picked 
+    * how 
+        - we can add event listener
+
+- 3 divs to tell if the player win or loss 
+    * why
+        - displays the score state to the user so they are able to tell where theu are at in the process of the game
+    * how 
+        - we can update the state with text Content on each button click
+
+
+
+# JS Events Setup
+
+- Click on the cups
+ - reset state
+    * why
+        - so the game has its own reset feature
+    * how 
+        - reset the images to hidden 
+
+
+  - place the state
+     * why
+        - so we keep track of events
+    * how 
+        - using math random
+
+
+  - update DOM
+     * why
+        - helps user to know who wins 
+    * how 
+        -  changing image of correct cup 
+        - change numbers of wins and losses 
+
+
+
+
+
--- app.js
+++ app.js
@@ -1,8 +1,103 @@
 // import functions and grab DOM elements
 
 // let state
+const cup1Btn = document.getElementById('cup1');
+const cup2Btn = document.getElementById('cup2');
+const cup3Btn = document.getElementById('cup3');
 
-// set event listeners 
+const resetBtn = document.getElementById('hardReset');
+
+
+const cup1Container = document.getElementById('cup1Contain');
+const cup2Container = document.getElementById('cup2Contain');
+const cup3Container = document.getElementById('cup3Contain');
+
+
+const winsEl = document.getElementById('win');
+const lossesEl = document.getElementById('loss');
+const totalEl = document.getElementById('total');
+
+const hidingPlaces = [
+    'cup1',
+    'cup2',
+    'cup3'
+];
+
+let win = 0;
+let losses = 0;
+let totalGuesses = 0;
+
+function resetStyle() {
+    cup1Container.src = './assets/non-active.png';
+    cup2Container.src = './assets/non-active.png';
+    cup3Container.src = './assets/non-active.png';
+}
+
+function getRandomItem(arr) {
+    const index = Math.floor(Math.random() * arr.length);
+
+    return arr[index];
+}
+
+
+function handleGuess(userGuess, correctGuess) {
+// should reset the styles
+    setTimeout(resetStyle, 500);
+    // then increment the guesses
+    totalGuesses++;
+    // then grab the appropriate container element for the correct guess from the DOM
+    let correctHiding = document.getElementById(`${correctGuess}Contain`);
+    // console.log(correctHiding);
+    // then add the face class to that element so that the face shows up
+    correctHiding.src = './assets/active.png';
+    // then if the user guess is correct, increment the correct guesses
+    if (userGuess === correctGuess) {
+        win++; 
+    } else {
+        losses++;
+    }
+    // update the DOM to show this change to the user (including the losses, not tracked directly in state)
+    totalEl.textContent = totalGuesses;
+    winsEl.textContent = win;
+    lossesEl.textContent = losses;
+
+ 
+}
+
+
+
+  
+  // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+cup1Btn.addEventListener('click', () => {
+    
+    let chooseRand = getRandomItem(hidingPlaces);
+    // console.log(chooseRand, 'choosen');
+    handleGuess('cup1', chooseRand);
+});
+
+
+cup2Btn.addEventListener('click', () => {
+    let chooseRand = getRandomItem(hidingPlaces);
+    // console.log(chooseRand, 'choosen');
+    handleGuess('cup2', chooseRand);
+});
+
+
+cup3Btn.addEventListener('click', () => {
+    let chooseRand = getRandomItem(hidingPlaces);
+    // console.log(chooseRand, 'choosen');
+    handleGuess('cup3', chooseRand);
+});
+
+resetBtn.addEventListener('click', () => {
+    resetStyle();
+    win = 0;
+    losses = 0;
+    totalGuesses = 0;
+    totalEl.textContent = totalGuesses;
+    winsEl.textContent = win;
+    lossesEl.textContent = losses;
+});
